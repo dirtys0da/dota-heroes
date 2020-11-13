652c61680bbb1b26baca6a65a1d274aefeb64fff
Ignacio Macam Jr <ignaciomacam@gmail.com>
2020-11-13 16:56:16 +0800

    added php
    checks the validity of documents

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$file = 'heroes.xml';
+$schema = 'heroes.xsd';
+$ab = new DOMDocument;
+$ab->load($file);
+
+if($ab->schemaValidate($schema)){
+    print "$file is valid.\n";
+}else{
+    print "$file is invalid.\n";
+}
+
+?>
